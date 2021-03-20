4689d45ec9d2467133282cd311d3a06c4326aca0
Julian Diepold <julian.diepold@hotmail.com>
2021-03-20 13:14:30 -0300

    /  Primera versión

---
+++ src/ColoresRGB/Principal.java
@@ -0,0 +1,9 @@
+
+package ColoresRGB;
+
+public class Principal {
+    public static void main(String[] args) {
+        Ventana ventana = new Ventana();
+        ventana.setVisible(true);
+    }
+}
